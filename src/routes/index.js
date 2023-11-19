7f825bebc48306b8ce96468986e8c607ec18a621
pedrodecf <contato.pedrodecf@gmail.com>
2023-11-19 18:03:49 -0300

    adicionar controle de sessão e rotas de sessão

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
-const { Router } = require("express");
-const usersRoutes = require("./users.routes");
-const movie_notesRoutes = require("./notes.routes");
-const movie_tagsRoutes = require("./tags.routes");
+const { Router } = require("express")
+const usersRoutes = require("./users.routes")
+const movie_notesRoutes = require("./notes.routes")
+const movie_tagsRoutes = require("./tags.routes")
+const sessionsRoutes = require("./sessions.routes")
 
-const routes = Router();
+const routes = Router()
 
-routes.use("/users", usersRoutes);
-routes.use("/movie", movie_notesRoutes);
-routes.use("/tags", movie_tagsRoutes);
+routes.use("/users", usersRoutes)
+routes.use("/sessions", sessionsRoutes)
+routes.use("/movie", movie_notesRoutes)
+routes.use("/tags", movie_tagsRoutes)
 
-module.exports = routes;
+module.exports = routes
