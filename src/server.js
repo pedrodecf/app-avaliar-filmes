5361a9d03de21f1386bf469004ffb596854711f9
pedrodecf <contato.pedrodecf@gmail.com>
2023-11-22 14:42:31 -0300

    adicionar sistema de upload de avatar

--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ require("express-async-errors");
 const AppError = require("./utils/AppError")
 const express = require("express");
 const routes = require("./routes")
+const uploadConfig = require("./configs/upload")
+
+const cors = require("cors")
 
 const api = express();
+api.use(cors())
+api.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
 api.use(express.json());
 api.use(routes);
