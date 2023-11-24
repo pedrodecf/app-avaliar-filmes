5e19ee47f60c04a50ad35c5c00929193be52a0b1
pedrodecf <contato.pedrodecf@gmail.com>
2023-11-23 23:14:54 -0300

    feat: adicionar dotenv e PM2

--- src/server.js
+++ src/server.js
@@ -1,30 +1,31 @@
-require("express-async-errors");
+require("express-async-errors")
+require("dotenv/config")
 const AppError = require("./utils/AppError")
-const express = require("express");
+const express = require("express")
 const routes = require("./routes")
 const uploadConfig = require("./configs/upload")
 
 const cors = require("cors")
 
-const api = express();
+const api = express()
 api.use(cors())
 api.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
-api.use(express.json());
-api.use(routes);
-api.use(( error, request, response, next ) => {
-    if (error instanceof AppError) {
-        return response.status(error.statusCode).json({
-            status: "error",
-            message: error.message
-        });
-    };
+api.use(express.json())
+api.use(routes)
+api.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    })
+  }
 
-        return response.status(500).json({
-        status: "error",
-        message: "Internal server error"
-    });
-});
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  })
+})
 
-const PORT = 3333;
-api.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+const PORT = process.env.SERVER_PORT || 3000
+api.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
