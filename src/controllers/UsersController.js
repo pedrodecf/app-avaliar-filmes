2b514044acdc67e32691cb69d83b2c1aca839292
pedrodecf <contato.pedrodecf@gmail.com>
2023-11-24 23:13:50 -0300

    test: adicionando test p/ UserCreate

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,74 +1,72 @@
-const AppError = require("../utils/AppError");
-const sqliteConnection = require("../database/sqlite");
-const { hash, compare } = require("bcryptjs");
+const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite")
+const { hash, compare } = require("bcryptjs")
+const UserRepository = require("../repositories/UserRepository")
+const UserCreateService = require("../services/UserCreateService")
 
 class UsersController {
-    async create(request, response) {
-        const { name, email, password } = request.body;
-        const database = await sqliteConnection();
-        const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+  async create(request, response) {
+    const { name, email, password } = request.body
 
-        if (checkUserExist) {
-            throw new AppError("Este e-mail já está em uso.")
-        };
+    const userRepository = new UserRepository()
+    const userCreateService = new UserCreateService(userRepository)
 
-        const hashedPassword = await hash(password, 8);
+    await userCreateService.execute({ name, email, password})
 
-        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-        [ name, email, hashedPassword ]
-        );
+    return response.status(201).json()
+  }
 
-        return response.status(201).json();
-    };
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body
+    const user_id = request.user.id
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [
+      user_id,
+    ])
 
-    async update(request, response) {
-        const { name, email, password, old_password } = request.body;
-        const user_id = request.user.id
-        const database = await sqliteConnection();
-        const user = await database.get("SELECT * FROM users WHERE id = (?)", [
-          user_id,
-        ])
-        
-        if (!user) {
-            throw new AppError("Usuário não encontrado.");
-        };
+    if (!user) {
+      throw new AppError("Usuário não encontrado.")
+    }
 
-        const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+    const userWithUpdateEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    )
 
-        if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
-            throw new AppError("Este em-mail já está em uso.");
-        };
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+      throw new AppError("Este em-mail já está em uso.")
+    }
 
-        user.name = name ?? user.name;
-        user.email = email ?? user.email;
+    user.name = name ?? user.name
+    user.email = email ?? user.email
 
-        if (password && !old_password) {
-            throw new AppError("Você precisa informar sua senha antiga.");
-        };
+    if (password && !old_password) {
+      throw new AppError("Você precisa informar sua senha antiga.")
+    }
 
-        if (password && old_password) {
-            const checkOldPassword = await compare(old_password, user.password);
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
 
-            if(!checkOldPassword) {
-                throw new AppError("A senha antiga não confere.");
-            }
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga não confere.")
+      }
 
-            user.password = await hash(password, 8);
-        };
+      user.password = await hash(password, 8)
+    }
 
-        await database.run(
-          `
+    await database.run(
+      `
             UPDATE users SET
             name = ?,
             email = ?,
             password = ?,
             updated_at = DATETIME('now')
             WHERE id = ?`,
-          [user.name, user.email, user.password, user_id]
-        )
+      [user.name, user.email, user.password, user_id]
+    )
 
-        return response.json();
-    }
-};
+    return response.json()
+  }
+}
 
-module.exports = UsersController;
+module.exports = UsersController
